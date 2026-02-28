610f05e651914d5d03fae2980683b2219c901212
vadiqboooo <vadiqbozhko@gmail.com>
2026-02-28 16:26:48 +0800

    fix server

--- server/database.py
+++ server/database.py
@@ -1,7 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./coffee_shop.db"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", "sqlite:///./data/coffee_shop.db"
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
